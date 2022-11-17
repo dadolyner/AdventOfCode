e16751228f939e321d1e82e06f80cf53f3caa074
dadolyner <skulj.david@gmail.com>
2022-11-17 19:44:56 +0100

    refactor day3

--- 2021/Day_3/script.ts
+++ 2021/Day_3/script.ts
@@ -1,44 +1,34 @@
 import PuzzleInput from './data'
 
 // Part 1
-type TReport = { gammaRate: string, epsilonRate: string }
-const Report: TReport = { gammaRate: '', epsilonRate: '' } as TReport
-
-const powerConsumptionFrequency = (currentBitPos: number) => {
+const PartOne = () => {
     let onBitCount = 0, offBitCount = 0, gammaRate = '', epsilonRate = ''
-    for (let i = 0; i < PuzzleInput.length; i++) {
-        const bit = PuzzleInput[i].charAt(currentBitPos)
-        if (bit === '1') onBitCount++
-        else offBitCount++
+    for (let col = 0; col < PuzzleInput[0].length; col++) {
+        for (let row = 0; row < PuzzleInput.length; row++) {
+            const bit = PuzzleInput[row].charAt(col)
+            if (bit === '1') onBitCount++
+            else offBitCount++
+        }
+
+        if (onBitCount > offBitCount) {
+            gammaRate += '1'
+            epsilonRate += '0'
+        } else {
+            gammaRate += '0'
+            epsilonRate += '1'
+        }
+
+        onBitCount = 0
+        offBitCount = 0
     }
 
-    if (onBitCount > offBitCount) {
-        gammaRate = '1'
-        epsilonRate = '0'
-    } else {
-        gammaRate = '0'
-        epsilonRate = '1'
-    }
-
-    return { gammaRate, epsilonRate }
-}
-
-const calculatePowerConsumption = () => {
-    let currentBitPos = 0
-    for (let i = 0; i < PuzzleInput[0].length; i++) {
-        const { gammaRate, epsilonRate } = powerConsumptionFrequency(currentBitPos)
-        Report.gammaRate += gammaRate
-        Report.epsilonRate += epsilonRate
-        currentBitPos++
-    }
-
-    const decimalGammaRate = parseInt(Report.gammaRate, 2)
-    const decimalEpsilonRate = parseInt(Report.epsilonRate, 2)
+    const decimalGammaRate = parseInt(gammaRate, 2)
+    const decimalEpsilonRate = parseInt(epsilonRate, 2)
     const powerConsumption = decimalGammaRate * decimalEpsilonRate
 
     return powerConsumption
 }
-console.log('Power consumption rating: ', calculatePowerConsumption())
+console.log('Power consumption rating: ', PartOne())
 
 // Part 2
 const getRating = (type: 'OxygenGenerator' | 'CO2Scrubber') => {
@@ -71,12 +61,12 @@ const getRating = (type: 'OxygenGenerator' | 'CO2Scrubber') => {
     return { binary, decimal }
 }
 
-const calculateLifeSupportRating = () => {
-    const ogRating = getRating('OxygenGenerator')
-    const co2Rating = getRating('CO2Scrubber')
+const PartTwo = () => {
+    const oxygenGeneratorRating = getRating('OxygenGenerator')
+    const co2ScrubberRating = getRating('CO2Scrubber')
 
-    const lifeSupportRating = ogRating.decimal * co2Rating.decimal
+    const lifeSupportRating = oxygenGeneratorRating.decimal * co2ScrubberRating.decimal
     return lifeSupportRating
 }
 
-console.log('Life support rating: ', calculateLifeSupportRating())
+console.log('Life support rating: ', PartTwo())
