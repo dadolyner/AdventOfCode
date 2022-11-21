c69c639a0db5b380a2f1149868155a754612c2f9
dadolyner <skulj.david@gmail.com>
2022-11-21 14:29:53 +0100

    i give up on this

--- 2021/Day_4/script.ts
+++ 2021/Day_4/script.ts
@@ -1,16 +1,13 @@
 import PuzzleInput from './puzzle_input'
 
 // Types
-type TBoards = { boardIndex: number, id: string, board: string[], drawnNum: number, isWinner: boolean }[]
+type TBoards = { boardIndex: number, id: string, board: string[], drawnNum: number, hasWon: boolean }[]
 
 const Boards: TBoards = []
 const winners: TBoards = []
 class SubmarineBingo {
-    numberOfBoards: number = 0
     lastDrawnNumber: number = 0
-    constructor(numberOfBoards: number) {
-        this.numberOfBoards = numberOfBoards
-    }
+    constructor() { }
 
     // Create unique ID for each board
     private genUuid = async (): Promise<string> => {
@@ -22,8 +19,14 @@ class SubmarineBingo {
     }
     // Generate boards from string input
     public genBingoBoards = async (): Promise<TBoards> => {
-        for (let i = 0; i < this.numberOfBoards; i++) {
-            Boards.push({ boardIndex: i, id: await this.genUuid(), board: PuzzleInput.boards[i].split(" "), drawnNum: 0, isWinner: false })
+        for (let i = 0; i < PuzzleInput.boards.length; i++) {
+            Boards.push({
+                boardIndex: i + 1,
+                id: await this.genUuid(),
+                board: PuzzleInput.boards[i].split(" "),
+                drawnNum: 0,
+                hasWon: false
+            })
             const currentBoard = Boards[i].board
             const boardGrid: Array<string> = [] // @ts-ignore
             for (let j = 0; j < 5; j++) boardGrid.push(currentBoard.slice(j * 5, j * 5 + 5))
@@ -35,7 +38,7 @@ class SubmarineBingo {
     private checkRowBingo = async (board: Array<string>): Promise<boolean | undefined> => {
         for (let row = 0; row < 5; row++) {
             for (let col = 0; col < 5; col++) {
-                if (!board[row][col].startsWith("(") && !board[row][col].endsWith(")")) break
+                if (!board[row][col].startsWith("(") && !board[row][col].endsWith(")")) return false
                 if (col === 4) return true
             }
         }
@@ -44,13 +47,13 @@ class SubmarineBingo {
     private checkColumnBingo = async (board: Array<string>): Promise<boolean | undefined> => {
         for (let col = 0; col < 5; col++) {
             for (let row = 0; row < 5; row++) {
-                if (!board[row][col].startsWith("(") && !board[row][col].endsWith(")")) break
+                if (!board[row][col].startsWith("(") && !board[row][col].endsWith(")")) return false
                 if (row === 4) return true
             }
         }
     }
     // Calculate score -> (unchecked numbers * last drawn number)
-    public calculateScore = async (board: Array<string>, number?: number) => {
+    public calculateScore = async (board: Array<string>, number: number) => {
         let sumUnmarked = 0
         for (let row = 0; row < 5; row++) {
             for (let col = 0; col < 5; col++) {
@@ -65,7 +68,7 @@ class SubmarineBingo {
     public drawNumber = async (number: number) => {
         for (let board = 0; board < Boards.length; board++) {
             const currentBoard = Boards[board]
-            if(currentBoard.isWinner === true) continue
+            if (currentBoard.hasWon === true) continue
             for (let row = 0; row < 5; row++) {
                 for (let col = 0; col < 5; col++) {
                     if (+currentBoard.board[row][col] === number) { //@ts-ignore
@@ -73,14 +76,17 @@ class SubmarineBingo {
                     }
                 }
             }
+        }
+        for (let board = 0; board < Boards.length; board++) {
+            const currentBoard = Boards[board]
+            if (currentBoard.hasWon === true) continue
             const rowWinner = await this.checkRowBingo(currentBoard.board)
             const colWinner = await this.checkColumnBingo(currentBoard.board)
-            if ((rowWinner === true || colWinner === true) && currentBoard.isWinner === false) {
+            if (rowWinner === true || colWinner === true) {
                 currentBoard.drawnNum = number
-                currentBoard.isWinner = true
-                winners.push(currentBoard)
+                currentBoard.hasWon = true
                 this.lastDrawnNumber = number
-                return { boardIndex: board, currentBoard }
+                return { boardIndex: board + 1, currentBoard }
             }
         }
     }
@@ -88,16 +94,15 @@ class SubmarineBingo {
 
 // Part 1
 const PartOne = async () => {
-    const boardCount = PuzzleInput.boards.length
-    const game = new SubmarineBingo(boardCount)
+    const game = new SubmarineBingo()
     await game.genBingoBoards()
 
     for (let i = 0; i < PuzzleInput.numbers.length; i++) {
         const number = PuzzleInput.numbers[i]
         const winner = await game.drawNumber(number)
         if (winner) {
-            const score = await game.calculateScore(winner.currentBoard.board)
-            console.log(`Board ${winner.boardIndex + 1} won on drawing number ${winner.currentBoard.drawnNum}, with score: ${score}`)
+            const score = await game.calculateScore(winner.currentBoard.board, winner.currentBoard.drawnNum)
+            console.log(`Board ${winner.boardIndex} won on drawing number ${winner.currentBoard.drawnNum}, with score: ${score}`)
             break
         }
     }
@@ -106,21 +111,20 @@ const PartOne = async () => {
 
 // Part 2
 const PartTwo = async () => {
-    const boardCount = PuzzleInput.boards.length
-    const game = new SubmarineBingo(boardCount)
+    const game = new SubmarineBingo()
     await game.genBingoBoards()
 
     for (let i = 0; i < PuzzleInput.numbers.length; i++) {
         const number = PuzzleInput.numbers[i]
         const winner = await game.drawNumber(number)
-        if (winner) {
-            const score = await game.calculateScore(winner.currentBoard.board)
-            console.log(`Board ${winner.boardIndex + 1} won on drawing number ${winner.currentBoard.drawnNum}, with score: ${score}`)
+        if(winner) {
+            const winnerExists = winners.find(w => w.id === winner.currentBoard.id)
+            if(!winnerExists && winner.currentBoard.hasWon) winners.push(winner.currentBoard)
         }
     }
 
     const lastWinner = winners[winners.length - 1]
     const score = await game.calculateScore(lastWinner.board, lastWinner.drawnNum)
-    console.log(`The final score is: ${score}`)
+    console.log(`Board ${lastWinner.boardIndex} won on drawing number ${lastWinner.drawnNum}, with score: ${score}`)
 }
 PartTwo()
