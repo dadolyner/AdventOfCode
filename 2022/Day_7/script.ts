3705701afd27ebb115d805cf897dd95594e1de2e
dadolyner <skulj.david@gmail.com>
2022-12-08 09:17:32 +0100

    object circular loop :)

--- 2022/Day_7/script.ts
+++ 2022/Day_7/script.ts
@@ -1,34 +1,44 @@
-import PuzzleInput from './test_data'
+import PuzzleInput from './puzzle_input'
 
+type DataObj = { [key: string]: any[] }
 const PartOne = () => {
-    const FileSystem = {}
-    const puzzleData = PuzzleInput
-    const filteredData = puzzleData.filter(pi => !pi.includes('$ ls'))
-    
-    let parentDirectory = '', currentDirectory = ''
-    for (let i = 0; i < filteredData.length; i++) {
-        if(filteredData[i].includes('$ cd')) {
-            const Directory = filteredData[i].split(' ')[2]
-            if(Directory === '..') {
-                currentDirectory = parentDirectory
-                continue
-            }
-            currentDirectory = Directory
-            Object.assign(FileSystem, {[currentDirectory]: []})
+    const data = PuzzleInput
+    const dataObj: DataObj = {}
+    let currentDir = ''
+    data.forEach((line) => {
+        if (line.startsWith('$ cd')) currentDir = line.split(' ')[2]
+        else if (line.startsWith('dir')) {
+            const dir = line.split(' ')[1]
+            if (dataObj[currentDir]) dataObj[currentDir].push(dir)
+            else dataObj[currentDir] = [dir]
+        } else if (line.match(/\d/)) {
+            const file = line.split(' ')[0]
+            if (dataObj[currentDir]) dataObj[currentDir].push(+file)
+            else dataObj[currentDir] = [+file]
         }
+    })
 
-        if(!isNaN(parseInt(filteredData[i][0]))) {
-            const FileSize = filteredData[i].split(' ')[0]
-            const FileName = filteredData[i].split(' ')[1]
-            const File = {
-                Name: FileName,
-                Size: +FileSize,
+    while (Object.values(dataObj).some((item) => item.some((item) => typeof item === 'string'))) {
+        for (const [key, value] of Object.entries(dataObj)) {
+            if (value.some((item) => typeof item === 'string')) {
+                value.forEach((item) => {
+                    if (typeof item === 'string') {
+                        const index = dataObj[key].indexOf(item)
+                        dataObj[key][index] = dataObj[item]
+                    }
+                })
             }
-            FileSystem[currentDirectory].push(File)
+            dataObj[key] = dataObj[key].flat()
         }
-
     }
 
-    console.log(FileSystem)
+    let totalSize = 0
+    for (const [key, value] of Object.entries(dataObj)) {
+        if (value.every((item) => item <= 100000)) {
+            totalSize += value.reduce((a, b) => a + b, 0)
+        }
+    }
+
+    console.log(totalSize)
 }
 PartOne()
