1fbc4b69b411b2e7f2464ae2fb94fd16c1525522
dadolyner <skulj.david@gmail.com>
2023-12-02 13:54:02 +0100

    updates to setup script

--- SETUP.bat
+++ SETUP.bat
@@ -1,17 +1,20 @@
 @echo off
-
-@echo This script generate folders for each day of advent calendar.
 set /P fileName=Enter file name:
 mkdir %fileName%
 cd %fileName%
 
 for /l %%x in (1, 1, 25) do (
     mkdir Day_%%x
-	cd Day_%%x
+    cd Day_%%x
+    
+    @echo 2> puzzle_input.txt
 	@echo 2> README.md
+    
 	@echo 2> script.ts
-	@echo 2> puzzle_input.txt
-	cd ..
+    @echo import { usePuzzleInput } from "../utils"; >> script.ts
+    @echo // @ts-ignore >> script.ts
+    @echo const PuzzleInput = usePuzzleInput(__dirname); >> script.ts
+    cd ..
 )
 
 pause
