1b0c6867f87302d194cfff44edc5838e15ec3eda
dadolyner <skulj.david@gmail.com>
2023-12-02 11:51:57 +0100

    refactor to class

--- 2023/Day_2/script.ts
+++ 2023/Day_2/script.ts
@@ -9,27 +9,27 @@ class DayTwo {
 
     // Helper function get data from a line in document
     private useLineData = (line: string) => {
-        let [game, sets] = line.split(': ');
-        let gameId = +game.match(/(\d+$)/)[0]
-        let set = sets.split('; ');
-        let possible = true;
-        let bagMin = { red: 0, green: 0, blue: 0 };
-        return { game, sets, gameId, set, possible, bagMin }
+        const [game, sets] = line.split(': ');
+        const gameId = +game.match(/(\d+$)/)[0]
+        const set = sets.split('; ');
+        const possible = true;
+        const bagMax = { red: 12, green: 13, blue: 14 };
+        const bagMin = { red: 0, green: 0, blue: 0 };
+        return { game, sets, gameId, set, possible, bagMin, bagMax }
     }
 
     // Part 1
     private PartOne(document: string[]) {
         let sumOfIds = 0;
-        const Bag = { red: 12, green: 13, blue: 14 };
 
         document.forEach((line: string) => {
-            let { gameId, set, possible } = this.useLineData(line);
+            let { gameId, set, possible, bagMax } = this.useLineData(line);
 
             set.forEach((item: string) => {
                 let cubes = item.split(", ");
                 cubes.forEach((cube: string) => {
                     let [count, color] = cube.split(' ');
-                    if (+count > Bag[color]) possible = false
+                    if (+count > bagMax[color]) possible = false
                 });
             });
 
